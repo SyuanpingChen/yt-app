68c8447858eac7d3d5c5685f8a3856299cd5f9c0
Ping Chen <ping840812@gmail.com>
2022-05-19 23:45:20 +0800

    correct

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const SearchBar = (onFormSubmit) => {
+const SearchBar = ({ onFormSubmit }) => {
 	const [term, setTerm] = useState("");
 
 	const onSubmit = (e) => {
